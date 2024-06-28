309b5319008fb49bfb82d2941533321d794b4ccc
Pierre-Hugues Husson <phh@phh.me>
2024-06-28 05:20:47 -0400

    We want a good reproduction of speech even if it lowers the noise suppression, so add speech=>speech in dataset

--- m_dataset.py
+++ m_dataset.py
@@ -6,8 +6,9 @@ import torch
 def samples(sample_bases, folder):
     s = [sample_bases + '/' + folder + '/' + x for x in os.listdir(sample_bases + '/' + folder)]
     # Filter out files
-    s = [x for x in s if os.path.isdir(x)]
-    s = [(x + '/mix.wav', x + '/speech.wav') for x in s]
+    sdirs = [x for x in s if os.path.isdir(x)]
+    s = [(x + '/mix.wav', x + '/speech.wav') for x in sdirs]
+    s += [(x + '/speech.wav', x + '/speech.wav') for x in sdirs]
     return s
 
 
